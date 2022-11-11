71c88218468a2bfcb521429a11dcc0f9fc2ae45a
Leomendferre <leo.mend@hotmail.com>
2022-11-11 14:55:59 +0000

    "(11/11) Adicionado Cors"

--- app.js
+++ app.js
@@ -20,7 +20,10 @@ catch (error) {
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-var app = express();
+var app = express(); 
+
+const cors = require('cors')
+app.use(cors())
 
 app.use(logger('dev'));
 app.use(express.json());
